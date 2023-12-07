b14665c01503bf31443b256c15a92065f6042028
Gary Kovar <gkovar@gmail.com>
2023-12-07 13:44:03 +0000

    end state

--- livewire/app/Livewire/CoinFlip.php
+++ livewire/app/Livewire/CoinFlip.php
@@ -6,13 +6,15 @@ use Livewire\Component;
 
 class CoinFlip extends Component
 {
+
+    public string $coin = 'Always tails!';
+
+    public function mount() {
+        $this->coin = rand(0, 1) ? 'Heads!' : 'Tails!';
+    }
+
     public function render()
     {
-        return view(
-            'livewire.coin-flip',
-            [
-                'coin' => rand(0, 1) ? 'heads' : 'tails'
-            ]
-        );
+        return view('livewire.coin-flip');
     }
 }
